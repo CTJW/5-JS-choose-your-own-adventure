d88783a24b9e1feff8e1daa5253ca8f1105c3621
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-01-11 20:35:26 -0500

    Stuck! Unable to get other prompts to work.

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+alert('Let\'s mix colors!')
+var colorOption = prompt('Pick a color... blue, red, or yellow')
+
+if (colorOption === 'blue') {
+   var redCOLOR = prompt('Pick the color that mixes with blue to make purple') }
+  
+
+   
+   if (redCOLOR === 'red') {
+        alert('Congratulations! You know your colors!')
+        var playAgain = prompt('Play Again? [Yes/No]') }
+   
+
+
+
+if
+    (colorOption === 'red') {
+    var yellowColor = prompt('Pick the color that mixes with red to make orange') }
+
+       if (yellowcolor === 'yellow') {
+            alert('Congratulations! You know your colors!')
+            var playAgain = prompt('Play Again? [Yes/No]') }
+
+
+
+if  
+    (colorOption === 'yellow') {
+    var blueColor = prompt('Pick the color that mixes with yellow to make green') }
+
+        if(blueColor === 'blue') {
+            alert('Congratulations! You know your colors!')
+            var playAgain = prompt('Play Again? [Yes/No]') }
+
+
+
+
+
+
+   
